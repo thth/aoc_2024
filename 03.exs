f449c6a2e676ddba0ee71502197c119c3655f57c
thth <ththfh@gmail.com>
2024-12-02 21:43:42 -0800

    line breaks ;_;

--- 03.exs
+++ 03.exs
@@ -2,25 +2,27 @@ defmodule Three do
   def part_one(input) do
     input
     |> parse()
+    |> then(&Regex.scan(~r/mul\((\d+)\,(\d+)\)/, &1))
+    |> Enum.map(fn [_, x, y] ->
+      String.to_integer(x) * String.to_integer(y)
+    end)
+    |> Enum.sum()
   end
-  
+
   def part_two(input) do
     input
     |> parse()
+    |> String.replace(~r/don't\(\).*?do\(\)/, "🐸")
+    |> part_one()
   end
 
   defp parse(text) do
     text
-    |> String.trim()
-    # |> String.split(~r/\R/)
-    # |> Enum.map(fn line ->
-    #   line
-    # end)
+    |> String.replace(~r/\R/, "")
   end
 end
 
 input = File.read!("input/03.txt")
-# input = File.read!("input/_test.txt")
 
 input |> Three.part_one() |> IO.inspect(label: "part 1")
-# input |> Three.part_two() |> IO.inspect(label: "part 2")
+input |> Three.part_two() |> IO.inspect(label: "part 2")
